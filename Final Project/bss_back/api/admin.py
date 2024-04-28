66399c62bb82993f1cdcc66b3682afd0bd3e2a31
San671 <85022684+San671@users.noreply.github.com>
2024-04-28 16:54:16 +0500

    Delete Final Project directory

--- Final Project/bss_back/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from api.models import Category, Product, Order, Comment
-
-admin.site.register(Category)
-admin.site.register(Product)
-
-admin.site.register(Order)
-admin.site.register(Comment)
